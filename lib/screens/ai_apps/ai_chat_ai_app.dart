e357772800ce328e842c2baeb3515ace1a3832b7
tspaja2025 <tspaja2025@outlook.com>
2026-01-27 08:49:15 +0200

    Update ai_chat_ai_app.dart

--- lib/screens/ai_apps/ai_chat_ai_app.dart
+++ lib/screens/ai_apps/ai_chat_ai_app.dart
@@ -9,9 +9,37 @@ class AiChatAiAppScreen extends StatelessWidget {
       padding: const .all(16),
       child: Column(
         children: [
-          Row(children: [const Text("AI Chat App").bold().large()]),
+          Row(children: [const Text("AI Chat").bold().large()]),
 
           const SizedBox(height: 16),
+
+          SizedBox(
+            width: 800,
+            height: MediaQuery.of(context).size.height - 150,
+            child: Column(
+              mainAxisAlignment: .center,
+              children: [
+                Row(
+                  spacing: 8,
+                  children: [
+                    IconButton.outline(
+                      onPressed: () {},
+                      icon: const Icon(LucideIcons.paperclip),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        placeholder: const Text("Ask me anything..."),
+                      ),
+                    ),
+                    IconButton.outline(
+                      onPressed: () {},
+                      icon: const Icon(LucideIcons.send),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
