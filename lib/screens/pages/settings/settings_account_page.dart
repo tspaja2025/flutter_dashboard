8ca9868f6f1798d50289eeb9df61e2f07e085917
tspaja2025 <tspaja2025@outlook.com>
2026-02-10 09:26:25 +0200

    Update settings_account_page.dart

--- lib/screens/pages/settings/settings_account_page.dart
+++ lib/screens/pages/settings/settings_account_page.dart
@@ -1,7 +1,16 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-class SettingsAccountPageScreen extends StatelessWidget {
+class SettingsAccountPageScreen extends StatefulWidget {
   const SettingsAccountPageScreen({super.key});
+
+  @override
+  State<SettingsAccountPageScreen> createState() =>
+      SettingsAccountPageScreenState();
+}
+
+class SettingsAccountPageScreenState extends State<SettingsAccountPageScreen> {
+  String? selectedValue;
+  DateTime? _value;
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +18,123 @@ class SettingsAccountPageScreen extends StatelessWidget {
       padding: const .all(16),
       child: Column(
         children: [
-          Row(children: [const Text("Settings Account Page").bold().large()]),
+          Center(
+            child: SizedBox(
+              width: 800,
+              child: Column(
+                crossAxisAlignment: .start,
+                children: [
+                  const Text("Settings Account Page").bold().large(),
+                  const SizedBox(height: 4),
+                  const Text(
+                    "Manage your account settings and set e-mail preferences.",
+                  ).muted().small(),
+                ],
+              ),
+            ),
+          ),
 
           const SizedBox(height: 16),
+
+          Center(
+            child: SizedBox(
+              width: 800,
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    FormField(
+                      key: FormKey(#name),
+                      label: const Text("Name"),
+                      child: TextField(placeholder: const Text("name")),
+                    ),
+                    const SizedBox(height: 4),
+                    const Text(
+                      "This is the name that will be displayed on your profile and in emails.",
+                    ).muted().small(),
+
+                    const SizedBox(height: 16),
+
+                    FormField(
+                      key: FormKey(#dateofbirth),
+                      label: const Text("Date of birth"),
+                      child: DatePicker(
+                        value: _value,
+                        mode: PromptMode.popover,
+                        stateBuilder: (date) {
+                          if (date.isAfter(DateTime.now())) {
+                            return DateState.disabled;
+                          }
+                          return DateState.enabled;
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            _value = value;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    const Text(
+                      "Your date of birth is used to calculate your age.",
+                    ).muted().small(),
+
+                    const SizedBox(height: 16),
+
+                    FormField(
+                      key: FormKey(#language),
+                      label: const Text("Language"),
+                      child: Select<String>(
+                        itemBuilder: (context, item) {
+                          return Text(item);
+                        },
+                        popupConstraints: const BoxConstraints(
+                          maxHeight: 300,
+                          maxWidth: 200,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedValue = value;
+                          });
+                        },
+                        value: selectedValue,
+                        placeholder: const Text("Select language"),
+                        popup: const SelectPopup(
+                          items: SelectItemList(
+                            children: [
+                              SelectItemButton(
+                                value: "English",
+                                child: Text("English"),
+                              ),
+                              SelectItemButton(
+                                value: "French",
+                                child: Text("French"),
+                              ),
+                              SelectItemButton(
+                                value: "German",
+                                child: Text("German"),
+                              ),
+                            ],
+                          ),
+                        ).call,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    const Text(
+                      "This is the language that will be used in the dashboard.",
+                    ).muted().small(),
+
+                    const SizedBox(height: 16),
+
+                    PrimaryButton(
+                      onPressed: () {},
+                      child: const Text("Update Account"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
