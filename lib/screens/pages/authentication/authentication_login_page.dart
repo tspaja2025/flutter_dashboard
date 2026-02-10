66cc4b27ee77383b5c2dcf2de63c1f98fc9d7a4a
tspaja2025 <tspaja2025@outlook.com>
2026-02-10 10:59:40 +0200

    Update authentication_login_page.dart

--- lib/screens/pages/authentication/authentication_login_page.dart
+++ lib/screens/pages/authentication/authentication_login_page.dart
@@ -12,6 +12,91 @@ class AuthenticationLoginPageScreen extends StatelessWidget {
           Row(children: [const Text("Login Page").bold().large()]),
 
           const SizedBox(height: 16),
+
+          Center(
+            child: SizedBox(
+              width: 400,
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    const Text("Welcome back").x4Large().bold(),
+                    const Text(
+                      "Please sign in to your account",
+                    ).muted().small(),
+
+                    const SizedBox(height: 16),
+
+                    TextField(placeholder: const Text("Email address")),
+                    const SizedBox(height: 16),
+                    TextField(placeholder: const Text("Password")),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: .end,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text("Forgot your password?"),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: PrimaryButton(
+                        onPressed: () {},
+                        child: const Text("Sign in"),
+                      ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Stack(
+                      alignment: .center,
+                      children: [
+                        const Divider(),
+                        const Text(
+                          "or continue with",
+                          style: TextStyle(backgroundColor: Colors.white),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Row(
+                      mainAxisAlignment: .center,
+                      spacing: 16,
+                      children: [
+                        OutlineButton(
+                          onPressed: () {},
+                          child: const Text("Google"),
+                        ),
+                        OutlineButton(
+                          onPressed: () {},
+                          child: const Text("Github"),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Row(
+                      mainAxisAlignment: .center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text("Don't have an account? Sign up"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
