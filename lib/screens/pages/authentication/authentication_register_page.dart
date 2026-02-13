74aba3285d79b8ebe337fb9c9c2808f0cc3c9b05
tspaja2025 <tspaja2025@outlook.com>
2026-02-13 18:09:01 +0200

    Update authentication_register_page.dart

--- lib/screens/pages/authentication/authentication_register_page.dart
+++ lib/screens/pages/authentication/authentication_register_page.dart
@@ -12,6 +12,82 @@ class AuthenticationRegisterPageScreen extends StatelessWidget {
           Row(children: [const Text("Register Page").bold().large()]),
 
           const SizedBox(height: 16),
+
+          Center(
+            child: SizedBox(
+              width: 400,
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    const Text("Create New Account").x4Large().bold(),
+
+                    const SizedBox(height: 16),
+
+                    TextField(placeholder: const Text("First name")),
+                    const SizedBox(height: 16),
+                    TextField(placeholder: const Text("Last name")),
+                    const SizedBox(height: 16),
+                    TextField(placeholder: const Text("Email address")),
+                    const SizedBox(height: 16),
+                    TextField(placeholder: const Text("Password")),
+
+                    const SizedBox(height: 16),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: PrimaryButton(
+                        onPressed: () {},
+                        child: const Text("Sign up"),
+                      ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Stack(
+                      alignment: .center,
+                      children: [
+                        const Divider(),
+                        const Text(
+                          "or continue with",
+                          style: TextStyle(backgroundColor: Colors.white),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Row(
+                      mainAxisAlignment: .center,
+                      spacing: 16,
+                      children: [
+                        OutlineButton(
+                          onPressed: () {},
+                          child: const Text("Google"),
+                        ),
+                        OutlineButton(
+                          onPressed: () {},
+                          child: const Text("Github"),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Row(
+                      mainAxisAlignment: .center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text("Already have an account? Sign in"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
