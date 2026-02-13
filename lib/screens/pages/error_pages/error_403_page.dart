6ff8cb3420ba929585b5d2d003408ae72f2ce89d
tspaja2025 <tspaja2025@outlook.com>
2026-02-13 18:18:59 +0200

    Update error_403_page.dart

--- lib/screens/pages/error_pages/error_403_page.dart
+++ lib/screens/pages/error_pages/error_403_page.dart
@@ -12,6 +12,32 @@ class Error403PageScreen extends StatelessWidget {
           Row(children: [const Text("Error 403 Page").bold().large()]),
 
           const SizedBox(height: 16),
+
+          OutlinedContainer(
+            width: 800,
+            height: 300,
+            backgroundColor: Colors.gray.shade100,
+            child: Column(
+              mainAxisAlignment: .center,
+              children: [
+                const Text("403").x8Large().bold(),
+                const SizedBox(height: 16),
+                const Text("No Authorization").x2Large(),
+                const SizedBox(height: 16),
+                const Text(
+                  "You do not appear to have permission to access this page",
+                ).muted().small(),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 16),
+
+          OutlineButton(
+            onPressed: () {},
+            trailing: const Icon(LucideIcons.arrowRight),
+            child: const Text("Back to Home"),
+          ),
         ],
       ),
     );
