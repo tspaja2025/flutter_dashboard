771b31ae67df53bfdd22bb6aa17f9fe94bd60723
tspaja2025 <tspaja2025@outlook.com>
2026-02-13 18:44:26 +0200

    Update pos_app.dart

--- lib/screens/apps/pos_app.dart
+++ lib/screens/apps/pos_app.dart
@@ -9,7 +9,27 @@ class PosAppScreen extends StatelessWidget {
       padding: const .all(16),
       child: Column(
         children: [
-          Row(children: [const Text("POS App").bold().large()]),
+          Row(
+            spacing: 8,
+            children: [
+              const Text("POS App").bold().large(),
+              const Spacer(),
+              OutlineButton(
+                onPressed: () {},
+                leading: const Icon(LucideIcons.plus),
+                child: const Text("Add Product"),
+              ),
+              OutlineButton(
+                onPressed: () {},
+                leading: const Icon(LucideIcons.filePen),
+                child: const Text("Tables"),
+              ),
+              IconButton.outline(
+                onPressed: () {},
+                icon: const Icon(LucideIcons.search),
+              ),
+            ],
+          ),
 
           const SizedBox(height: 16),
         ],
