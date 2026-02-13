c42d681af15034ba0045307e3e9122c50635c063
tspaja2025 <tspaja2025@outlook.com>
2026-02-13 18:16:06 +0200

    Update error_404_page.dart

--- lib/screens/pages/error_pages/error_404_page.dart
+++ lib/screens/pages/error_pages/error_404_page.dart
@@ -12,6 +12,28 @@ class Error404PageScreen extends StatelessWidget {
           Row(children: [const Text("Error 404 Page").bold().large()]),
 
           const SizedBox(height: 16),
+
+          OutlinedContainer(
+            width: 800,
+            height: 300,
+            backgroundColor: Colors.gray.shade100,
+            child: Column(
+              mainAxisAlignment: .center,
+              children: [
+                const Text("404").x8Large().bold(),
+                const SizedBox(height: 16),
+                const Text("Page Not Found").x2Large(),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 16),
+
+          OutlineButton(
+            onPressed: () {},
+            trailing: const Icon(LucideIcons.arrowRight),
+            child: const Text("Back to Home"),
+          ),
         ],
       ),
     );
