1afe31158d90017a302987968eca2e71a7c97a03
tspaja2025 <tspaja2025@outlook.com>
2026-02-13 18:37:38 +0200

    Update text_to_speech_ai_app.dart

--- lib/screens/ai_apps/text_to_speech_ai_app.dart
+++ lib/screens/ai_apps/text_to_speech_ai_app.dart
@@ -12,6 +12,43 @@ class TextToSpeechAiAppScreen extends StatelessWidget {
           Row(children: [const Text("Text To Speech App").bold().large()]),
 
           const SizedBox(height: 16),
+
+          OutlinedContainer(
+            width: 400,
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      spacing: 8,
+                      children: [
+                        CircularProgressIndicator(),
+                        const Text("Texth to Speech").large().semiBold(),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    const Text(
+                      "Text to speech app is a ui template that converts written text into spoken audio.",
+                    ),
+                  ],
+                ).withPadding(all: 16),
+
+                const SizedBox(height: 16),
+                const Divider(),
+                const SizedBox(height: 16),
+
+                Row(
+                  spacing: 8,
+                  children: [
+                    const Icon(LucideIcons.circleAlert),
+                    const Text("This page is currently under construction."),
+                  ],
+                ).withPadding(left: 16, right: 16, top: 0, bottom: 16),
+              ],
+            ),
+          ),
         ],
       ),
     );
